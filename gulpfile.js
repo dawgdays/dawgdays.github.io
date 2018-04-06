7d9784089a08b4a62739b6ad714ef0a5052cf4e3
Rich McLaughlin <rsmclaug@gmail.com>
2018-04-06 14:59:54 -0400

    added gulp-serve

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,22 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var watch = require('gulp-watch');
 var path = require('path');
+var serve = require('gulp-serve');
 
-gulp.task('default', ['less', 'watch']);
+gulp.task('default', ['less', 'serve', 'watch']);
 
 gulp.task('less', function () {
     return gulp.src('./src/styles/**/*.less')
-      .pipe(less({
-        paths: [ path.join(__dirname, 'less', 'includes') ]
-      }))
-      .pipe(gulp.dest('./public/styles'));
-  });
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(gulp.dest('./public/styles'));
+});
+
+gulp.task('serve', serve({
+    root: ['.'],
+    port: 3000,
+}));
 
 gulp.task('watch', ['less'], function () {
     gulp.watch('./src/styles/**/*.less', ['less'])
