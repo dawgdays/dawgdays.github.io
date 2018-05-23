26b55c7b02b8fcdb755a6b1dd2fb5f5b728a4ce5
Rich McLaughlin <rsmclaug@gmail.com>
2018-05-23 16:39:15 -0400

    alphabetize templates

--- public/scripts/app-templates.js
+++ public/scripts/app-templates.js
@@ -5,12 +5,12 @@ catch(err) { app = angular.module("app-templates", []); }
 app.run(["$templateCache", function($templateCache) {
 "use strict";
 
-$templateCache.put("/components/menu/menu.html","<h1>Menu!</h1>")
-
 $templateCache.put("/components/main/main.html","<div class=\"main\">\n" +
     "    <h1>Main!</h1>\n" +
     "\n" +
     "    {{main.test}}\n" +
     "</div>")
+
+$templateCache.put("/components/menu/menu.html","<h1>Menu!</h1>")
 }]);
 })();
